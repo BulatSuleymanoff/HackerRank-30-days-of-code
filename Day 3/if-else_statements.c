017f674569e5581e17825d02784fc94046b26215
BulatSuleymanoff <bulat.suleymanoff@gmail.com>
2016-01-04 20:23:53 +0500

    Add solution for Day 3 in C

---
+++ Day 3/if-else_statements.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int N; 
+    scanf("%d",&N);
+
+    if (N % 2)
+        printf("Weird");
+    else {
+        if ((N >= 6 && N <= 20))
+            printf("Weird");
+        else printf("Not Weird");
+    }
+    
+    return 0;
+}
