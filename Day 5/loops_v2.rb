45e60582571b8012cb622d0f3b747055d9eb9928
BulatSuleymanoff <bulat.suleymanoff@gmail.com>
2016-01-08 02:07:36 +0500

    Add solution with string for Day 5 in Ruby

---
+++ Day 5/loops_v2.rb
@@ -0,0 +1,14 @@
+ans = ""
+num_of_tests = gets().to_i()
+for test in 0..num_of_tests-1
+    str_in = gets().split(/ /).map!{ |o| o.to_i() }
+    a = str_in[0]
+    b = str_in[1]
+    n = str_in[2]
+    current_sum = a
+    for i in 0..n-1
+        current_sum += 2 ** i * b
+        ans += current_sum.to_s() + " "
+    end
+    ans += "\n"
+end
