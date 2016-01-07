ef590eaa9fcc091337ae91d08b3c99dd998f94e7
BulatSuleymanoff <bulat.suleymanoff@gmail.com>
2016-01-07 23:57:22 +0500

    Add solution without string for Day 5 in C#

---
+++ Day 5/loops_v1.cs
@@ -0,0 +1,18 @@
+using System;
+class Solution {
+    static void Main(String[] args) {
+        int numOfTests = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < numOfTests; ++i) {
+    	    string[] strIn = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(strIn[0]);
+            int b = Convert.ToInt32(strIn[1]);
+            int N = Convert.ToInt32(strIn[2]);
+    	    int currentSum = a;
+    	    for (int j = 0; j < N; ++j) {
+    		    currentSum += Convert.ToInt32(Math.Pow(2, j)) * b;
+    		    Console.Write(currentSum.ToString() + " ");
+    	    }
+    	    Console.WriteLine();
+        }
+    }
+}
